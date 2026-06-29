f8092fd76b3ee11692291b70b78190522332e4b8
Shiraj-Shrivastava <shirajlogin@gmail.com>
2026-06-29 20:53:06 +0530

    Added test column

--- models/example/city_count.sql
+++ models/example/city_count.sql
@@ -1,6 +1,5 @@
 {{config (materialized = 'view')}}
-
 select  count(*) as total_cities,
-        max(cityid) as max_cityid
+        max(cityid) as max_cityid,
+        'abc' as test_col
 from {{ref('city_details')}}
-
