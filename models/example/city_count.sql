7d44d277e03363ff86493f191d8da987f7cfaf8b
Shiraj-Shrivastava <shirajlogin@gmail.com>
2026-06-29 21:06:41 +0530

    Changed max to min and added blanks

--- models/example/city_count.sql
+++ models/example/city_count.sql
@@ -1,6 +1,10 @@
+
 {{config (materialized = 'view')}}
 
 select  count(*) as total_cities,
-        max(cityid) as max_cityid
+        min(cityid) as max_cityid
 from {{ref('city_details')}}
 
+
+
+    
